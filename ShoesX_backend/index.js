c43d92081cf0f689436d6e230d2df9bc8c2ead4e
hardiklakum20 <120251398+hardiklakum20@users.noreply.github.com>
2025-07-16 10:18:05 +0530

    make changes
    make changes

--- ShoesX_backend/index.js
+++ ShoesX_backend/index.js
@@ -40,3 +40,4 @@ app.use('/api', UserRouter);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port http://localhost:${PORT}`));
+module.exports.handler = serverless(app);
