21bc4227c067c97f8db504cf5dab090d9426d254
Lakum Hardik <120251398+hardiklakum20@users.noreply.github.com>
2025-07-16 10:31:52 +0530

    Update index.js

--- ShoesX_backend/index.js
+++ ShoesX_backend/index.js
@@ -45,4 +45,5 @@ app.use('/api', UserRouter);
 // const PORT = process.env.PORT || 5000;
 
 // app.listen(PORT, () => console.log(`Server started on port http://localhost:${PORT}`));
+module.exports = app;
 module.exports.handler = serverless(app);
